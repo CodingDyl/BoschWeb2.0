15748eebafa90a3271500f5609946712335fed04
Dylan Petzer <dylan.petzer@entelect.co.za>
2023-04-18 09:13:54 +0200

    base project structure developed, installed dependencies

---
+++ src/constants/index.js
@@ -0,0 +1,28 @@
+const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Dylan proved me wrong.",
+      name: "Peter Evgeniou",
+      designation: "CFO",
+      company: "Bosch Northcliff",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Kayla Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/woman/4.jpg",
+    },
+    {
+      testimonial:
+        "After Dylan optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+
+  export { testimonials };
